49e38e917dba20ffd28631402a72dd2c7bbeafe3
Restuta <restuta8@gmail.com>
2010-10-31 19:05:05 +0200

    Initial commit

---
+++ Src/StringCalculator.Core/StringCalculator.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StringCalculator.Core
+{
+    public class StringCalculator
+    {
+    }
+}
---
+++ Src/StringCalculator.Tests.MSpec/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StringCalculator.Tests.MSpec
+{
+    public class Class1
+    {
+    }
+}
