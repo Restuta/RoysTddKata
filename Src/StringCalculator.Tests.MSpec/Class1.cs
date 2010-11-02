84d07c545691ccca2c8cc6a20170f89fbd3eacc1
Restuta <restuta8@gmail.com>
2010-11-03 01:44:18 +0200

    Added one test in each test project

--- Src/StringCalculator.Tests.MSpec/Class1.cs
+++ Src/StringCalculator.Tests.MSpec/Class1.cs
@@ -2,10 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Machine.Specifications;
+using StringCalculator.Core;
 
 namespace StringCalculator.Tests.MSpec
 {
-    public class Class1
+    [Subject(typeof(SimpleStringCalculator))]
+    public class when_add_method_is_invoked
     {
+        private static SimpleStringCalculator simpleStringCalculator;
+        private static int result = -1;
+
+        Establish context =
+            () =>
+                {
+                    simpleStringCalculator = new SimpleStringCalculator();
+                };
+
+        Because of =
+            () => result = simpleStringCalculator.Add(string.Empty);
+
+        It should_return_a_zero = () => result.ShouldEqual(0);
     }
 }
